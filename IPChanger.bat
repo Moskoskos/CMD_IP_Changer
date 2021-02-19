5e250db1141ac5676cc1d921159094ef94b2bc9a
Moskoskos <atleag@outlook.com>
2021-02-19 15:13:33 +0100

    Update IPChanger.bat

--- IPChanger.bat
+++ IPChanger.bat
@@ -4,10 +4,16 @@ cls
 :start
 
 SET nic=Ethernet
+
 SET desc1="PLC Network"
-SET desc2="NAT outside"
 SET ip1=192.168.0.100
+SET sm1=255.255.255.0
+SET gw1=192.168.0.100
+
+SET desc2="NAT outside"
 SET ip2=192.168.12.100
+SET sm2=255.255.255.0
+SET gw2=192.168.12.100
  
 ECHO. 
 ECHO 1. %desc1%	%ip1%
@@ -30,14 +36,14 @@ goto start
 :: They will also change your IP to the IP stated after the word static
 :con1 
 ECHO Changing IP for %nic%...
-netsh interface ip set address %nic% static %ip1% 255.255.255.0 124.0.0.1 1 
+netsh interface ip set address %nic% static %ip1% %sm1% %gw1% 1 
 ECHO IP: %ip1%
 Pause 
 EXIT 
 
 :con2 
 ECHO Changing IP for %nic%...
-netsh interface ip set address %nic% static %ip2% 255.255.255.0 124.0.0.1 1 
+netsh interface ip set address %nic% static %ip2% %sm2% %gw2% 1 
 ECHO IP: %ip2% 
 Pause 
 EXIT 
